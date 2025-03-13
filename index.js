5dc845e6e5ba7769cf9b83dc391105bb5e03971a
matheusgondra <matheusgondra951@gmail.com>
2025-03-13 13:08:47 -0300

    feat: create eslint config

---
+++ index.js
@@ -0,0 +1,34 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node,
+      parser: "@typescript-eslint/parser",
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          tabWidth: 2,
+          useTabs: true,
+          trailingComma: "none",
+          arrowParens: "always",
+          printWidth: 120,
+        },
+      ],
+    },
+    files: ["src/**/*.{ts,tsx}", "test/**/*.{ts,tsx}"],
+    ignores: ["node_modules", "dist"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettier,
+];
